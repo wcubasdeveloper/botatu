bce699561d605c56592e1ff52602685a1f712bef
William Cubas Alegria <william.cubas@PROTRANSPORTES.LOCAL>
2019-10-28 10:13:56 -0500

    segundo commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var request = require('request');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 80;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,20 +24,29 @@ app.post('/webhook', (req, res) => {
   //var textoRecibido = req.body.queryResult.parameters["echoText"];
   console.log(req);
   console.log("mensaje recibidooo");
-  request('http://softbar.e-docperu.com/Registros/procGeneralWebHook?parametros=m|1&nombreProcedimiento=ProcProducto&indice=19', function (error, response, body) {
-    var rptaJSON = JSON.parse(body);
-    var productosEncontrados = "";
-    rptaJSON.forEach(element => { 
-      productosEncontrados += "\n- " + element.NomProducto + ' S/. *' + element.PrecioVenta + '*';
-    });
-    let respuestaParaChat = {
-      "fullfillmentText" : "rpta",
-      "fulfillmentMessages" : [{"text":{"text": [productosEncontrados] }}],
-      // "fulfillmentMessages" : [{"text":{"text": ["7777","jjejejej"] }}, {"text":{"text": ["555","rffdd"] }}],
-      "source": ""
-    };
-    res.json(respuestaParaChat);
-  });
+
+  let respuestaParaChat = {
+    "fullfillmentText" : "rpta",
+    "fulfillmentMessages" : [{"text":{"text": [ "hola desde el NODEJS"] }}],
+    // "fulfillmentMessages" : [{"text":{"text": ["7777","jjejejej"] }}, {"text":{"text": ["555","rffdd"] }}],
+    "source": ""
+  };
+  res.json(respuestaParaChat);
+
+  // request('http://softbar.e-docperu.com/Registros/procGeneralWebHook?parametros=m|1&nombreProcedimiento=ProcProducto&indice=19', function (error, response, body) {
+  //   var rptaJSON = JSON.parse(body);
+  //   var productosEncontrados = "";
+  //   rptaJSON.forEach(element => { 
+  //     productosEncontrados += "\n- " + element.NomProducto + ' S/. *' + element.PrecioVenta + '*';
+  //   });
+  //   let respuestaParaChat = {
+  //     "fullfillmentText" : "rpta",
+  //     "fulfillmentMessages" : [{"text":{"text": [productosEncontrados] }}],
+  //     // "fulfillmentMessages" : [{"text":{"text": ["7777","jjejejej"] }}, {"text":{"text": ["555","rffdd"] }}],
+  //     "source": ""
+  //   };
+  //   res.json(respuestaParaChat);
+  // });
 });
 
 
